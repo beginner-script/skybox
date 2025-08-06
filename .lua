8db44eef8f6007a7cb8cf9f854a2f57cba8c581f
No_dev <bagsinjae80@gmail.com>
2025-08-07 02:10:22 +0900

    madebyNo_1.lua

---
+++ .lua
@@ -0,0 +1,19 @@
+local Lighting = game:GetService("Lighting")
+
+for _, v in pairs(Lighting:GetChildren()) do
+    if v:IsA("Sky") then
+        v:Destroy()
+    end
+end
+
+local sky = Instance.new("Sky")
+local imgId = "rbxassetid://104219751230869"
+
+sky.SkyboxBk = imgId
+sky.SkyboxDn = imgId
+sky.SkyboxFt = imgId
+sky.SkyboxLf = imgId
+sky.SkyboxRt = imgId
+sky.SkyboxUp = imgId
+
+sky.Parent = Lighting
